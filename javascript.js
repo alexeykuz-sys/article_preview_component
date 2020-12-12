848d20dc6b0f0f7c904e6036a8766ffa1b6773fb
Alexey Kuzmis <alexeykuzmis@Alexeys-MacBook-Pro.local>
2020-12-12 21:46:30 +0000

    added js to all buttons

--- javascript.js
+++ javascript.js
@@ -4,14 +4,36 @@ const CARDFOOTER =document.getElementById('card-footer');
 const CARDFOOTERGREY =document.getElementById('card-footer-grey');
 
 function footerHandler (){
-    if (CARDFOOTER.classList.contains('show')){
-        CARDFOOTER.classList.remove('show')
-        CARDFOOTER.classList.add('none')
-        CARDFOOTERGREY.classList.add('show')
-    } else if (CARDFOOTERGREY.classList.contains('show')){
-        CARDFOOTERGREY.classList.remove('show')
-        CARDFOOTERGREY.classList.add('none')
-        CARDFOOTER.classList.add('show')
+    if(screen.width<700){
+        if (CARDFOOTER.classList.contains('show')){
+            CARDFOOTER.classList.remove('show');
+            CARDFOOTER.classList.add('none');
+            CARDFOOTERGREY.classList.add('show');
+        } else if (CARDFOOTERGREY.classList.contains('show')){
+            
+            CARDFOOTERGREY.classList.remove('show');
+            CARDFOOTERGREY.classList.add('none');
+            CARDFOOTER.classList.add('show');
+        } 
+    }else if (screen.width > 700){
+        
+        if(SHARESIGN.classList.contains('show')){
+            CARDFOOTERGREY.classList.add('show');
+            SHARESIGN.classList.remove('show');
+            SHARESIGN2.classList.add('show');
+            
+        } else if (SHARESIGN2.classList.contains('show')){
+            CARDFOOTERGREY.classList.remove('show');
+            CARDFOOTERGREY.classList.add('none');
+            SHARESIGN.classList.remove('none');
+            SHARESIGN.classList.add('show');
+            SHARESIGN2.classList.remove('none');
+            SHARESIGN2.classList.add('none');
+            
+            
+            
+        }
+        
     }
 }
 
